0d66b0d6db05157f2984fe8e6f3cbd9668e33821
Наталия <105249744+NataliyaChyi@users.noreply.github.com>
2022-07-20 22:03:09 +0300

    extracting data from the database

--- vebsite/main/models.py
+++ vebsite/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
 # Create your models here.
--- vebsite/main/urls.py
+++ vebsite/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about_us', views.about_us),
+    path('', views.index, name='home'),
+    path('about_us', views.about_us, name='about'),
 ]
