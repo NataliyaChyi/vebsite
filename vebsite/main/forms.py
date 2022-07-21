5df8c7bc4a5e8075d39287a944b59e447a70ae84
Наталия <105249744+NataliyaChyi@users.noreply.github.com>
2022-07-21 23:11:07 +0300

    add working form in field in examples

--- vebsite/main/forms.py
+++ vebsite/main/forms.py
@@ -1,10 +1,10 @@
 from . models import Task
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput, Textarea
 
 class TaskForm(ModelForm):
         class Meta:
             model = Task
             fields = ["title", "task"]
             widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'}),
-                       "task": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите описание'}),
+                       "task": Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите описание'}),
             }
